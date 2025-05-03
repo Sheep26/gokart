2a8bf45b02a42df80a54c7cae2fe6e75a1af54fd
Sheep26 <crabeykiller@gmail.com>
2025-05-03 21:00:27 +1200

    Update renderer.js

--- app/src/renderer.js
+++ app/src/renderer.js
@@ -1,43 +1,45 @@
 class DATA {
-    constructor(speed = 0, rpm = 0, power = 0, throttle = 0, api_url = "") {
-        this.speed = speed;
-        this.rpm = rpm;
-        this.power = power;
-        this.throttle = throttle;
+    constructor(api_url = "") {
+        this.data = null;
+        this.api_url = api_url;
     }
 
     async update_data() {
         // Implementation for updating data
 
         try {
-            const response = await fetch(url);
-            
+            // Request data from api
+            const response = await fetch(this.api_url + "/api/exported_data");
+
             if (!response.ok) {
               throw new Error(`Response status: ${response.status}`);
             }
         
             const json = await response.json();
+
+            // Set data variable
+            this.data = json;
         } catch (error) {
             console.error(error.message);
         }
     }
 
     get_speed() {
-        return this.speed
+        return this.data.speed
     }
 
     get_rpm() {
-        return this.rpm
+        return this.data.rpm
     }
 
     get_power() {
-        return this.power
+        return this.data.power
     }
 
     get_throttle() {
-        this.throttle
+        this.data.throttle
     }
 }
 
-var data = new DATA();
+var data = new DATA("https://gokart.sheepland.xyz");
 
