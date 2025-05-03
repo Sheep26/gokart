0e6b1fb987b68938eaf52e242901b625a12ab7d6
Sheep26 <crabeykiller@gmail.com>
2025-05-04 10:54:35 +1200

    send api requests

--- service/main.cpp
+++ service/main.cpp
@@ -5,14 +5,49 @@
 #include <thread>
 #include <chrono>
 #include <curl/curl.h>
+#include <format>
 
 using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
 
 void data_thread() {
+    // Send data to server every 100ms
     while (true) {
-        
+        CURL* curl = curl_easy_init();
+        string response;
+
+        if (curl) {
+            std::string body = std::format(R"({{
+                "speed": {},
+                "speed_avg": {},
+                "speed_max": {},
+                "rpm": {},
+                "rpm_avg": {},
+                "rpm_max": {},
+                "power": {},
+                "power_avg": {},
+                "power_max": {},
+                "throttle": {},
+                "throttle_avg": {},
+                "throttle_max": {}
+            }})", data.speed.current, data.speed.avg, data.speed.max, data.rpm.current, data.rpm.avg, data.rpm.max, data.power.current, data.power.avg, data.power.max, data.throttle.current, data.throttle.avg, data.throttle.max);
+            const char* body_cstr = body.c_str();
+
+            curl_easy_setopt(curl, CURLOPT_URL, "https://gokart.sheepland.xyz/apt/update_data");
+            curl_easy_setopt(curl, CURLOPT_POST, 1L);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body_cstr);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(body_cstr));
+    
+            CURLcode res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+    
+            if (res == CURLE_OK) {
+                std::cout << "Response:\n" << response << std::endl;
+            } else {
+                std::cerr << "Request failed: " << curl_easy_strerror(res) << std::endl;
+            }
+        }
 
         sleep_for(milliseconds(100));
     }
