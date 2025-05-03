a503306da54db96a474743821faab0b1b77a0778
Sheep26 <crabeykiller@gmail.com>
2025-05-04 09:52:07 +1200

    e

--- service/main.cpp
+++ service/main.cpp
@@ -7,29 +7,13 @@
 
 using namespace std;
 using namespace std::this_thread;
-using namespace std::chrono
+using namespace std::chrono;
 
-int main() {
-    cout << "Starting gokart deamon.";
+void data_thread() {
 
-    // Setup GPIO
-    // Uses BCM numbering of the GPIOs and directly accesses the GPIO registers.
-    wiringPiSetupGpio();
-
-    // Create a thread for ffmpeg video feed.
-    thread ffmpeg_t(ffmpeg_setup);
-    ffmpeg_t.join();
-
-    // Main loop.
-    while (true) {
-        sleep_for(seconds(0.032));
-    }
-
-    // Return 0.
-    return 0;
 }
 
-void ffmpeg_setup() {
+void ffmpeg_thread() {
     // Start ffmpeg.
     cout << "Starting ffmpeg live video feed.";
 
@@ -37,4 +21,32 @@ void ffmpeg_setup() {
     if (ret != 0) {
         cerr << "Error: ffmpeg command failed with exit code " << ret << endl;
     }
+}
+
+int main() {
+    cout << "Starting gokart deamon.";
+
+    // Setup GPIO
+    // Uses BCM numbering of the GPIOs and directly accesses the GPIO registers.
+    if (wiringPiSetupGpio() == -1) {
+        cerr << "Error: Failed to initialize GPIO." << endl;
+        return 1;
+    }
+
+    // Create a thread for ffmpeg video feed.
+    thread ffmpeg_t(ffmpeg_thread);
+    ffmpeg_t.join();
+
+    // Create a thread to upload the data to the server.
+    thread data_t(data_thread);
+    data_t.join();
+
+    // Main loop.
+    while (true) {
+        sleep_for(milliseconds(33));
+        break;
+    }
+
+    // Return 0.
+    return 0;
 }
