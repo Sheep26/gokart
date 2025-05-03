5037a20fa266997438a62d2e6cefd4fcb85c0595
Sheep26 <crabeykiller@gmail.com>
2025-05-03 23:08:01 +1200

    remove mediasoup and codec

--- service/main.cpp
+++ service/main.cpp
@@ -25,7 +25,7 @@ void ffmpeg_setup() {
     // Start ffmpeg
     cout << "Starting ffmpeg live video feed.";
 
-    int ret = system("ffmpeg -f v4l2 -i /dev/video0 -vcodec libx264 -f flv rtmp://gokart.sheepland.xyz/apt/videofeed");
+    int ret = system("ffmpeg -f v4l2 -i /dev/video0 -f flv rtmp://gokart.sheepland.xyz/apt/videofeed");
     if (ret != 0) {
         cerr << "Error: ffmpeg command failed with exit code " << ret << endl;
     }
