686e9ac34e02a57f38778db4dd1a872feced9aac
Sheep26 <crabeykiller@gmail.com>
2025-05-05 17:49:30 +1200

    Update compile.sh

--- service/compile.sh
+++ service/compile.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 
 # Check dependencies.
-if ! [$(apt list | grep -q git)]; then
+if ! $(apt list | grep -q git); then
     echo "Dependency git missing, installing."
     # Install libcurl library
     apt update
     apt install git -y
 fi
 
-if ! [$(apt list | grep -q g++)]; then
+if ! $(apt list | grep -q g++); then
     echo "Build tools missing, installing."
     # Install build-essential (includes g++)
     apt update
     apt install build-essential -y
 fi
 
-if ! [$(apt list | grep -q wiringPi)]; then
+if ! $(apt list | grep -q wiringPi); then
     echo "Dependency wiringPi missing, installing."
     git clone https://github.com/WiringPi/WiringPi.git
     # Install
@@ -30,7 +30,7 @@ if ! [$(apt list | grep -q wiringPi)]; then
     rm -rf WiringPi
 fi
 
-if ! [$(apt list | grep -q libcurl4-openssl-dev)]; then
+if ! $(apt list | grep -q libcurl4-openssl-dev); then
     echo "Dependency libcurl4-openssl-dev missing, installing."
     # Install libcurl library
     apt update
