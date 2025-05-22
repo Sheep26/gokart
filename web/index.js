07b15f3bf7297bc2d6d8c9efa9e8977b06339b8d
Sheep26 <crabeykiller@gmail.com>
2025-05-22 20:17:07 +1200

    hls

--- web/index.js
+++ web/index.js
@@ -11,18 +11,33 @@ fs.readFile('config.json', 'utf8', function (err, data) {
     json_config = JSON.parse(data);
 });
 
+const transformationConfig = {
+  ffmpeg: "ffmpeg",
+  tasks: [
+    {
+      app: "live",
+      hls: true,
+      hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+      hlsKeep: false,
+    },
+  ],
+  MediaRoot: "/media",
+};
+
 const config = {
     rtmp: {
         port: 1935,
         chunk_size: 60000,
         gop_cache: true,
-        ping: 30,
+        ping: 10,
         ping_timeout: 60
     },
     http: {
         port: 8002,
-        allow_origin: '*'
-    }
+        allow_origin: '*',
+        mediaroot: '/media'
+    },
+    trans: transformationConfig
 };
 
 // Encode and parse data.
