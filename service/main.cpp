1e209b344ed857fa0b8cd0506d1114b51a2ba1d6
Sheep26 <crabeykiller@gmail.com>
2025-08-07 20:19:31 +1200

    Update main.cpp

--- service/main.cpp
+++ service/main.cpp
@@ -94,8 +94,8 @@ void Threads::data_t() {
             break;
         }
  
-        // Sleep for 500ms
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        // Sleep for 100ms
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
