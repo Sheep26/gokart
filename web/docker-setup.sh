a549627b0bec81b93575530bd3eb1042ec75b801
Sheep26 <crabeykiller@gmail.com>
2025-08-08 12:12:42 +1200

    e

--- web/docker-setup.sh
+++ web/docker-setup.sh
@@ -1,6 +1,6 @@
 docker build -t gokart ./
 if [[ "$*" == *-d* ]]; then
-    docker run -p 1935:1935 -p 8001:8001 -p 8002:8002 -d gokart
+    docker run --name gokart -p 1935:1935 -p 8001:8001 -p 8002:8002 -d gokart
 else
-    docker run -p 1935:1935 -p 8001:8001 -p 8002:8002 gokart
+    docker run --name gokart -p 1935:1935 -p 8001:8001 -p 8002:8002 gokart
 fi
