22a954a55316e2883f42161e68c3169ab57908d2
Sheep26 <crabeykiller@gmail.com>
2025-09-12 09:35:15 +1200

    Update main.cpp

--- service/main.cpp
+++ service/main.cpp
@@ -253,6 +253,7 @@ void Threads::bluetooth_server() {
             cmd.erase(cmd.find_last_not_of("\r\n") + 1);
             std::vector<std::string> parts = split_string(cmd, ' ');
             std::vector<std::string> args(parts.begin() + 1, parts.end());
+            std::cout << args << "\n";
             std::string response = commandListener.handle_command(args);
             std::cout << "[BT]" << response << "\n";
             write(client_sock, response.c_str(), response.length());
