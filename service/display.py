a2758b9c588ec590e566580b3b0348d938ed9bd3
Sheep26 <crabeykiller@gmail.com>
2025-10-09 23:50:41 +1300

    Update display.py

--- service/display.py
+++ service/display.py
@@ -2,7 +2,7 @@ from luma.core.interface.serial import spi
 from luma.oled.device import ssd1309
 from luma.core.render import canvas
 
-serial = spi(device=0, port=0, gpio_DC=25, gpio_RST=24)
+serial = spi(device=0, port=0, gpio_DC=27, gpio_RST=22)
 device = ssd1309(serial, width=128, height=64)
 
 with canvas(device) as draw:
