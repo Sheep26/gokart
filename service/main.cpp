21497dc0ca98888b36f432d09b34fb93ef59c8a9
Sheep26 <crabeykiller@gmail.com>
2025-10-10 23:57:32 +1300

    Update main.cpp

--- service/main.cpp
+++ service/main.cpp
@@ -169,7 +169,7 @@ void Threads::ffmpeg_t() {
     // 720p@60
     std::string cmd =
         "ffmpeg -f v4l2 -framerate 30 -video_size 1280x720 -i /dev/video0 "
-        "-b:v 3M -maxrate 3M -c:v libx264 -bufsize 6M"
+        "-b:v 2M -maxrate 2M -c:v h264_omx -bufsize 4M"
         "-vf \"drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:"
         "textfile=/tmp/ffmpeg_overlay.txt:reload=1:x=10:y=10:fontsize=12:fontcolor=white:box=1:boxcolor=black@0.50\" "
         "-f flv rtmp://" + server.rtmp_ip + ":1935/live/stream?id=" + server.id + "&session=" + server.session;
